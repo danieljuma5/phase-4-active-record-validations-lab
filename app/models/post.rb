7c8740d3a84bcffa37b15da6c2dfc5af090e3e91
danieljuma5 <jumadaniel275@gmail.com>
2023-06-21 08:59:19 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: {within: ["Fiction","Non-Fiction"]}
+  validate :dont_allow_true_facts
+  # Custom Validation
+
+  def dont_allow_true_facts
+    if title == "True Facts"
+      errors.add(:title, "Title cannot be True Facts")
+    end
+  end
+
 end
